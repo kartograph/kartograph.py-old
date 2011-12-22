801243824bddee9b94fcf693818657e93517b2bc
Gregor Aisch <contact@vis4.net>
2011-12-22 23:55:43 +0100

    added flipped mode for upsidedown world maps

--- lib/proj.py
+++ lib/proj.py
@@ -52,6 +52,9 @@ class Proj(object):
 		if ignore:
 			return None
 		return [points]
+		
+	def ll(self, lon, lat):
+		return (lon,lat)
 	
 	def project(self, lon, lat):
 		assert False, 'Proj is an abstract class'
@@ -125,9 +128,9 @@ class Proj(object):
 		
 class Cylindrical(Proj):
 
-	def __init__(self, lon0 = 0.0):
+	def __init__(self, lon0 = 0.0, flip = 0):
+		self.flip = flip
 		self.lon0 = lon0
-		
 		sea = []
 		for lat in range(-90,90): sea.append((-180,lat))
 		for lon in range(-180,180): sea.append((lon, 90))
@@ -141,9 +144,7 @@ class Cylindrical(Proj):
 			
 	def plot(self, polygon, truncate=True):
 		if self.lon0 != 0.0:
-			
 			polygons = self._shift_polygon(polygon)
-			
 			plotted = []
 			for polygon in polygons:
 				plotted += super(Cylindrical, self).plot(polygon, False)
@@ -195,22 +196,28 @@ class Cylindrical(Proj):
 	def toXML(self):
 		p = super(Cylindrical, self).toXML()
 		p['lon0'] = str(self.lon0)
+		p['flip'] = str(self.flip)
 		return p
 		
 	def __str__(self):
 		return 'Proj('+self.name+', lon0=%s)' % self.lon0
 
+	def ll(self, lon, lat):
+		if self.flip == 1:
+			return (-lon, -lat)
+		return (lon,lat)
 
 class Equirectangular(Cylindrical):
 	"""
 	Equirectangular Projection, aka lonlat, aka plate carree
 	"""
-	def __init__(self, lon0 = 0.0, lat0 = 0.0):
+	def __init__(self, lon0 = 0.0, lat0 = 0.0, flip = 0):
 		self.lat0 = lat0
 		self.phi0 = rad(lat0 * -1)
-		Cylindrical.__init__(self, lon0 = lon0)
+		Cylindrical.__init__(self, lon0 = lon0, flip = flip)
 	
 	def project(self, lon, lat):
+		lon,lat = self.ll(lon,lat)
 		return (lon * math.cos(self.phi0)*1000, lat*-1*1000)
 
 projections['lonlat'] = Equirectangular
@@ -220,15 +227,16 @@ class CEA(Cylindrical):
 	"""
 	Cylindrical Equal Area Projection
 	"""
-	def __init__(self, lat0 = 0.0, lon0 = 0.0, lat1 = 0.0):
+	def __init__(self, lat0 = 0.0, lon0 = 0.0, lat1 = 0.0, flip = 0):
 		self.lat0 = lat0
 		self.lat1 = lat1
 		self.phi0 = rad(lat0 * -1)
 		self.phi1 = rad(lat1 * -1)
 		self.lam0 = rad(lon0)
-		Cylindrical.__init__(self, lon0 = lon0)
+		Cylindrical.__init__(self, lon0 = lon0, flip = flip)
 		
 	def project(self, lon, lat):
+		lon,lat = self.ll(lon, lat)
 		lam = rad(lon)
 		phi = rad(lat*-1)
 		x = (lam) * math.cos(self.phi1) * 1000
@@ -247,41 +255,42 @@ projections['cea'] = CEA
 
 
 class GallPeters(CEA):
-	def __init__(self, lat0 = 0.0, lon0=0.0):
-		CEA.__init__(self, lon0=lon0, lat0=0, lat1=45)
+	def __init__(self, lat0 = 0.0, lon0=0.0, flip = 0):
+		CEA.__init__(self, lon0=lon0, lat0=0, lat1=45, flip = flip)
 
 projections['gallpeters'] = GallPeters
 
 
 class HoboDyer(CEA):
-	def __init__(self, lat0=0.0, lon0=0.0):
-		CEA.__init__(self, lon0=lon0, lat0=lat0, lat1=37.5)
+	def __init__(self, lat0=0.0, lon0=0.0, flip = 0):
+		CEA.__init__(self, lon0=lon0, lat0=lat0, lat1=37.5, flip = flip)
 
 projections['hobodyer'] = HoboDyer
 
 
 class Behrmann(CEA):
-	def __init__(self, lat0 = 0.0, lon0=0.0):
-		CEA.__init__(self, lat1=30, lat0=lat0, lon0=lon0)
+	def __init__(self, lat0 = 0.0, lon0=0.0, flip = 0):
+		CEA.__init__(self, lat1=30, lat0=lat0, lon0=lon0, flip = flip)
 
 projections['behrmann'] = Behrmann
 
 
 class Balthasart(CEA):
-	def __init__(self, lat0 = 0.0, lon0=0.0):
-		CEA.__init__(self, lat1=50, lat0=lat0, lon0=lon0)
+	def __init__(self, lat0 = 0.0, lon0=0.0, flip = 0):
+		CEA.__init__(self, lat1=50, lat0=lat0, lon0=lon0, flip = flip)
 
 projections['balthasart'] = Balthasart
 
 
 class Mercator(Cylindrical):
 	
-	def __init__(self, lon0=0.0, lat0=0.0):
-		Cylindrical.__init__(self, lon0=lon0)
+	def __init__(self, lon0=0.0, lat0=0.0, flip = 0):
+		Cylindrical.__init__(self, lon0=lon0, flip = flip)
 		self.minLat = -85
 		self.maxLat = 85
 	
 	def project(self, lon, lat):
+		lon,lat = self.ll(lon, lat)
 		lam = rad(lon)
 		phi = rad(lat*-1)
 		x = lam * 1000
@@ -292,14 +301,14 @@ projections['mercator'] = Mercator
 
 
 class PseudoCylindrical(Cylindrical):
-	def __init__(self, lon0=0.0):
-		Cylindrical.__init__(self, lon0=lon0)
+	def __init__(self, lon0=0.0, flip = 0):
+		Cylindrical.__init__(self, lon0=lon0, flip = flip)
 
 
 class NaturalEarth(PseudoCylindrical):
 	
-	def __init__(self, lat0=0.0, lon0=0.0):
-		PseudoCylindrical.__init__(self, lon0=lon0)
+	def __init__(self, lat0=0.0, lon0=0.0, flip=0):
+		PseudoCylindrical.__init__(self, lon0=lon0, flip=flip)
 		from math import pi
 		s = self
 		s.A0 = 0.8707
@@ -322,6 +331,7 @@ class NaturalEarth(PseudoCylindrical):
 		
 	def project(self, lon, lat):
 		from math import radians as rad
+		lon,lat = self.ll(lon, lat)
 		lplam = rad(lon)
 		lpphi = rad(lat*-1)
 		phi2 = lpphi * lpphi
@@ -335,8 +345,8 @@ projections['naturalearth'] = NaturalEarth
 	
 class Robinson(PseudoCylindrical):
 
-	def __init__(self, lat0 = 0.0, lon0=0.0):
-		PseudoCylindrical.__init__(self, lon0=lon0)
+	def __init__(self, lat0 = 0.0, lon0=0.0, flip = 0):
+		PseudoCylindrical.__init__(self, lon0=lon0, flip = flip)
 		self.X = [1, -5.67239e-12, -7.15511e-05, 3.11028e-06,  0.9986, -0.000482241, -2.4897e-05, -1.33094e-06, 0.9954, -0.000831031, -4.4861e-05, -9.86588e-07, 0.99, -0.00135363, -5.96598e-05, 3.67749e-06, 0.9822, -0.00167442, -4.4975e-06, -5.72394e-06, 0.973, -0.00214869, -9.03565e-05, 1.88767e-08, 0.96, -0.00305084, -9.00732e-05, 1.64869e-06, 0.9427, -0.00382792, -6.53428e-05, -2.61493e-06, 0.9216, -0.00467747, -0.000104566, 4.8122e-06, 0.8962, -0.00536222, -3.23834e-05, -5.43445e-06, 0.8679, -0.00609364, -0.0001139, 3.32521e-06, 0.835, -0.00698325, -6.40219e-05, 9.34582e-07, 0.7986, -0.00755337, -5.00038e-05, 9.35532e-07, 0.7597, -0.00798325, -3.59716e-05, -2.27604e-06, 0.7186, -0.00851366, -7.0112e-05, -8.63072e-06, 0.6732, -0.00986209, -0.000199572, 1.91978e-05, 0.6213, -0.010418, 8.83948e-05, 6.24031e-06, 0.5722, -0.00906601, 0.000181999, 6.24033e-06, 0.5322,  0.,  0.,  0.]
 		self.Y = [0, 0.0124, 3.72529e-10, 1.15484e-09, 0.062, 0.0124001, 1.76951e-08, -5.92321e-09, 0.124, 0.0123998, -7.09668e-08, 2.25753e-08, 0.186, 0.0124008, 2.66917e-07, -8.44523e-08, 0.248, 0.0123971, -9.99682e-07, 3.15569e-07, 0.31, 0.0124108, 3.73349e-06, -1.1779e-06, 0.372, 0.0123598, -1.3935e-05, 4.39588e-06, 0.434, 0.0125501, 5.20034e-05, -1.00051e-05, 0.4968, 0.0123198, -9.80735e-05, 9.22397e-06, 0.5571, 0.0120308, 4.02857e-05, -5.2901e-06, 0.6176, 0.0120369, -3.90662e-05, 7.36117e-07, 0.6769, 0.0117015, -2.80246e-05, -8.54283e-07, 0.7346, 0.0113572, -4.08389e-05, -5.18524e-07, 0.7903, 0.0109099, -4.86169e-05, -1.0718e-06, 0.8435, 0.0103433, -6.46934e-05, 5.36384e-09, 0.8936, 0.00969679, -6.46129e-05, -8.54894e-06, 0.9394, 0.00840949, -0.000192847, -4.21023e-06, 0.9761, 0.00616525, -0.000256001, -4.21021e-06, 1.,  0.,  0.,  0]
 		self.NODES = 18
@@ -351,6 +361,7 @@ class Robinson(PseudoCylindrical):
 		return arr[off]+z * (arr[off+1]+z * (arr[off+2]+z * (arr[off+3])))
 
 	def project(self, lon, lat):
+		lon,lat = self.ll(lon, lat)
 		lplam = rad(lon)
 		lpphi = rad(lat*-1)
 
@@ -372,8 +383,8 @@ projections['robinson'] = Robinson
 
 class EckertIV(PseudoCylindrical):
 
-	def __init__(self, lon0=0.0, lat0=0):
-		PseudoCylindrical.__init__(self, lon0=lon0)
+	def __init__(self, lon0=0.0, lat0=0, flip = 0):
+		PseudoCylindrical.__init__(self, lon0=lon0, flip = flip)
 	
 		self.C_x = .42223820031577120149
 		self.C_y = 1.32650042817700232218
@@ -384,9 +395,9 @@ class EckertIV(PseudoCylindrical):
 		self.NITER = 6
 	
 	def project(self, lon, lat):
+		lon,lat = self.ll(lon, lat)
 		lplam = rad(lon)
 		lpphi = rad(lat*-1)
-		
 		
 		p = self.C_p * math.sin(lpphi)
 		V = lpphi * lpphi
@@ -415,10 +426,11 @@ projections['eckert4'] = EckertIV
 		
 class Sinusoidal(PseudoCylindrical):
 
-	def __init__(self, lon0=0.0, lat0=0.0):
-		PseudoCylindrical.__init__(self, lon0=lon0)
+	def __init__(self, lon0=0.0, lat0=0.0, flip = 0):
+		PseudoCylindrical.__init__(self, lon0=lon0, flip = flip)
 		
 	def project(self, lon, lat):
+		lon,lat = self.ll(lon, lat)
 		lam = rad(lon)
 		phi = rad(lat*-1)
 		x = lam * math.cos(phi)
@@ -430,8 +442,8 @@ projections['sinusoidal'] = Sinusoidal
 	
 class Mollweide(PseudoCylindrical):
 
-	def __init__(self, p=1.5707963267948966, lon0=0.0, lat0=0.0, cx=None, cy=None, cp=None):
-		PseudoCylindrical.__init__(self, lon0=lon0)
+	def __init__(self, p=1.5707963267948966, lon0=0.0, lat0=0.0, cx=None, cy=None, cp=None, flip = 0):
+		PseudoCylindrical.__init__(self, lon0=lon0, flip = flip)
 		self.MAX_ITER = 10
 		self.TOLERANCE = 1e-7
 		
@@ -450,6 +462,7 @@ class Mollweide(PseudoCylindrical):
 			assert False, 'either p or cx,cy,cp must be defined'
 		
 	def project(self, lon, lat):
+		lon,lat = self.ll(lon, lat)
 		lam = rad(lon)
 		phi = rad(lat)
 		
@@ -476,16 +489,16 @@ projections['mollweide'] = Mollweide
 	
 	
 class WagnerIV(Mollweide):
-	def __init__(self, lon0=0, lat0=0):
+	def __init__(self, lon0=0, lat0=0, flip = 0):
 		# p=math.pi/3
-		Mollweide.__init__(self, p=1.0471975511965976)
+		Mollweide.__init__(self, p=1.0471975511965976, flip = flip)
 
 projections['wagner4'] = WagnerIV
 
 		
 class WagnerV(Mollweide):
-	def __init__(self, lat0=0, lon0=0):
-		Mollweide.__init__(self, cx = 0.90977, cy = 1.65014, cp = 3.00896)
+	def __init__(self, lat0=0, lon0=0, flip = 0):
+		Mollweide.__init__(self, cx = 0.90977, cy = 1.65014, cp = 3.00896, flip = flip)
 
 projections['wagner5'] = WagnerV
 
@@ -495,13 +508,14 @@ class Loximuthal(PseudoCylindrical):
 	minLat = -89
 	maxLat = 89
 
-	def __init__(self, lon0=0.0, lat0=0.0):
-		PseudoCylindrical.__init__(self, lon0=lon0)
+	def __init__(self, lon0=0.0, lat0=0.0, flip = 0):
+		PseudoCylindrical.__init__(self, lon0=lon0, flip = flip)
+		if flip == 1: lat0 = -lat0
 		self.lat0 = lat0
 		self.phi0 = rad(lat0)
 		
 	def project(self, lon, lat):
-		
+		lon,lat = self.ll(lon, lat)
 		lam = rad(lon)
 		phi = rad(lat)
 		if phi == self.phi0:
@@ -597,6 +611,7 @@ class Orthographic(Azimuthal):
 		Azimuthal.__init__(self, lat0, lon0)		
 
 	def project(self, lon, lat):
+		lon,lat = self.ll(lon, lat)
 		elevation = self.to_elevation(lat)
 		azimuth = self.to_azimuth(lon)
 		xo = self.r*math.cos(elevation)*math.sin(azimuth-self.azimuth0)
@@ -622,7 +637,7 @@ class LAEA(Azimuthal):
 		
 	def project(self, lon, lat):
 		from math import radians as rad, pow, asin, cos, sin
-		
+		lon,lat = self.ll(lon, lat)
 		phi = rad(lat)
 		lam = rad(lon)
 		
@@ -656,7 +671,7 @@ class Stereographic(Azimuthal):
 		
 	def project(self, lon, lat):
 		from math import radians as rad, pow, asin, cos, sin
-		
+		lon,lat = self.ll(lon, lat)
 		phi = rad(lat)
 		lam = rad(lon)
 
@@ -709,7 +724,7 @@ class Satellite(Azimuthal):
 		
 	def project(self, lon, lat):
 		from math import radians as rad, pow, asin, cos, sin
-		
+		lon,lat = self.ll(lon, lat)
 		phi = rad(lat)
 		lam = rad(lon)
 
@@ -775,6 +790,7 @@ class EquidistantAzimuthal(Azimuthal):
 		
 	def project(self, lon, lat):
 		from math import radians as rad, pow, asin, cos, sin
+		lon,lat = self.ll(lon, lat)
 		
 		phi = rad(lat)
 		lam = rad(lon)
@@ -846,6 +862,7 @@ class LCC(Conic):
 		self.maxLat = 85
 		
 	def project(self, lon, lat):
+		lon,lat = self.ll(lon, lat)
 		phi = rad(lat)
 		lam = rad(lon)
 		n = self.n		
@@ -869,6 +886,7 @@ class Proj4(Proj):
 		self.proj = pyproj.Proj(projstr)
 		
 	def project(self, lon, lat):
+		lon,lat = self.ll(lon, lat)
 		x,y = self.proj(lon, lat)
 		return (x,y*-1)
 
