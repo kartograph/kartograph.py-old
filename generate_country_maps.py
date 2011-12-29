339c71cd2b20665dac6ec0accebb657ff0227a9d
Gregor Aisch <contact@vis4.net>
2011-12-29 11:49:24 +0100

    switched to AGPL

--- generate_country_maps.py
+++ generate_country_maps.py
@@ -4,12 +4,13 @@ import lib.proj
 
 countries = ["AGO", "BDI", "BEN", "BFA", "BWA", "CAF", "CIV", "CMR", "COD", "COG", "COM", "DJI", "DZA", "EGY", "ERI", "ETH", "FRA", "GAB", "GHA", "GIN", "GMB", "GNB", "GNQ", "KEN", "LBR", "LBY", "LSO", "MAR", "MDG", "MLI", "MOZ", "MRT", "MWI", "NAM", "NER", "NGA", "RWA", "SDN", "SEN", "SLE", "SOM", "STP", "SWZ", "SYC", "TCD", "TGO", "TUN", "TZA", "UGA", "ZAF", "ZMB", "ZWE"]
 
-countries = ["SSD"]
+countries = ["KEN"]
 
 opt = KartographOptions()
 
 opt.out_height = 700
 opt.out_width = 700
+opt.cut_lakes = True
 opt.round_coordinates = True
 opt.out_padding_perc = 0.05
 opt.projection = lib.proj.projections['laea']
