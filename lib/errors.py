e38ea76b82efa0c6ad2e53b4825467fcc4bd75b1
Gregor Aisch <contact@vis4.net>
2012-01-15 21:19:00 +0100

    wrapped error classes in seperate file

---
+++ lib/errors.py
@@ -0,0 +1,11 @@
+"""
+error classes for kartograph
+"""
+
+class OptionParseError(Exception):
+    """Base class for exceptions in this module."""
+    pass    
+
+
+class ShapefileAttributesError(Exception)
+	pass
